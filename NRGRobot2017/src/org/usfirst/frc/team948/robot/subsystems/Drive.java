750b3a4109128dc7d9ab54acc94ea94c5082bf25
SourishMurthy1 <murthy.sourish@outlook.com>
2017-01-16 14:43:48 -0800

    Drive Subsystem

---
+++ NRGRobot2017/src/org/usfirst/frc/team948/robot/subsystems/Drive.java
@@ -0,0 +1,18 @@
+package org.usfirst.frc.team948.robot.subsystems;
+
+import edu.wpi.first.wpilibj.command.Subsystem;
+
+/**
+ *
+ */
+public class Drive extends Subsystem {
+
+    // Put methods for controlling this subsystem
+    // here. Call these from Commands.
+
+    public void initDefaultCommand() {
+        // Set the default command for a subsystem here.
+        //setDefaultCommand(new MySpecialCommand());
+    }
+}
+
