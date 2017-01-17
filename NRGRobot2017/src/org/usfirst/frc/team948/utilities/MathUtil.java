23c097abcbd04d5eff643b904c92b18cf25d5009
gregoryquick <gregory@quicks.me>
2017-01-16 16:26:33 -0800

    Added MathUtil
    -Added utilities package
    -Added math util class with clamp and deadband methods

---
+++ NRGRobot2017/src/org/usfirst/frc/team948/utilities/MathUtil.java
@@ -0,0 +1,15 @@
+package org.usfirst.frc.team948.utilities;
+
+public class MathUtil {
+	public static double deadband(double input, double range){
+		return (input < 0 ? -input : input) < range ? 0 : input;
+	}
+	
+	public static double clampR(double input, double center, double range){
+		return input > center + range ? center + range : input < center - range ? center - range : input;
+	}
+	
+	public static double clampM(double input, double min, double max){
+		return input > max ? max : input < min ? min : input;
+	}
+}
