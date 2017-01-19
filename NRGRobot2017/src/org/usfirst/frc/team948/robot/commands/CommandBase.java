9f8776f9ff7e987ef0cc769e7e6544de91d5d4b8
Tejas2Rangole <tejas.rangole@gmail.com>
2017-01-18 19:31:19 -0800

    Created PreferenceKeys for DriveStraight

--- NRGRobot2017/src/org/usfirst/frc/team948/robot/commands/CommandBase.java
+++ NRGRobot2017/src/org/usfirst/frc/team948/robot/commands/CommandBase.java
@@ -4,13 +4,14 @@ import java.awt.Robot;
 
 import org.usfirst.frc.team948.robot.subsystems.Drive;
 
+import edu.wpi.first.wpilibj.Preferences;
 import edu.wpi.first.wpilibj.command.Command;
 
 /**
  *
  */
 public class CommandBase extends Command {
-
+	public static Preferences preferences = Preferences.getInstance();
     
 
     
--- NRGRobot2017/src/org/usfirst/frc/team948/utilities/PreferenceKeys.java
+++ NRGRobot2017/src/org/usfirst/frc/team948/utilities/PreferenceKeys.java
@@ -11,4 +11,7 @@ public class PreferenceKeys {
 	public static final String turnP = "turnP";
 	public static final String turnI = "turnI";
 	public static final String turnD = "turnD";
+	public static final String Drive_On_Heading_P = "DriveStraight_P";
+	public static final String Drive_On_Heading_I = "DriveStraight_I";
+	public static final String Drive_On_Heading_D = "DriveStraight_D";
 }
