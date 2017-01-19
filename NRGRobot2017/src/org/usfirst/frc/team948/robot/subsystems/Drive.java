4910f93fc245ace96970787e3f0cf4c65898dc7c
gregoryquick <gregory@quicks.me>
2017-01-18 17:06:05 -0800

    PID variables
    -Added PIDE nad PID variables in drive
    -renamed methods in drive

--- NRGRobot2017/src/org/usfirst/frc/team948/robot/subsystems/Drive.java
+++ NRGRobot2017/src/org/usfirst/frc/team948/robot/subsystems/Drive.java
@@ -2,31 +2,44 @@ package org.usfirst.frc.team948.robot.subsystems;
 
 import org.usfirst.frc.team948.robot.RobotMap;
 import org.usfirst.frc.team948.robot.commands.ManualDrive;
+import org.usfirst.frc.team948.utilities.PIDE;
 
+import edu.wpi.first.wpilibj.PIDController;
 import edu.wpi.first.wpilibj.command.Subsystem;
 
 /**
  *
  */
 public class Drive extends Subsystem {
-
+	private static PIDE sPIDE;
+	private static PIDController sPID;
     // Put methods for controlling this subsystem
     // here. Call these from Commands.
 
     public void initDefaultCommand() {
         setDefaultCommand(new ManualDrive());
     }
-    public void tankDrive(double leftPower, double rightPower){
+    public void rawTankDrive(double leftPower, double rightPower){
     	RobotMap.motorFrontLeft.set(leftPower);
-    	RobotMap.motorFrontRight.set(rightPower*-1);
+    	RobotMap.motorFrontRight.set(-rightPower);
     	RobotMap.motorBackLeft.set(leftPower);
-    	RobotMap.motorBackRight.set(rightPower*-1);
+    	RobotMap.motorBackRight.set(-rightPower);
     }
-    public void stop() {
+    public void rawStop() {
     	RobotMap.motorBackLeft.disable();
     	RobotMap.motorBackRight.disable();
     	RobotMap.motorFrontLeft.disable();
     	RobotMap.motorFrontRight.disable();
+    }
+    
+    public void intitTurnPID(){
+    	sPIDE = new PIDE("turn");
+    	sPID = sPIDE.con();
+    }
+    
+    public void initDrivePID(){
+       	sPIDE = new PIDE("drive");
+    	sPID = sPIDE.con();
     }
 }
 
