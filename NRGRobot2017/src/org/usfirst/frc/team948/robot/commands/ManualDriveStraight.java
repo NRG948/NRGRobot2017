0c4ba572e192c21cc623e2fb685b74e3d14162cd
Isabel O <kidlatmc@gmail.com>
2017-01-24 19:11:54 -0800

    #25 Create ManualDriveStraight Command

---
+++ NRGRobot2017/src/org/usfirst/frc/team948/robot/commands/ManualDriveStraight.java
@@ -0,0 +1,39 @@
+package org.usfirst.frc.team948.robot.commands;
+import org.usfirst.frc.team948.robot.Robot;
+import org.usfirst.frc.team948.robot.RobotMap;
+
+import edu.wpi.first.wpilibj.command.Command;
+
+public class ManualDriveStraight extends Command{
+	
+	
+	public ManualDriveStraight() {
+		requires(Robot.drive);
+	}
+
+	@Override 
+	protected void initialize() {
+		Robot.drive.driveOnHeadingInit(RobotMap.navx.getAngle());
+	}
+	
+	@Override 
+	protected void execute(){
+		Robot.drive.driveOnHeading(Robot.oi.leftJoystick.getY());
+	}
+	
+	@Override
+	protected boolean isFinished() {
+		return false;
+	}
+	
+	@Override 
+	protected void end() {
+		Robot.drive.driveOnHeadingEnd();
+	}
+	
+	@Override 
+	protected void interrupted() {
+		end(); 
+	}
+
+}
