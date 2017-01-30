152079a6026a8f27048989d63269b9928bae27f7
Isabel O <kidlatmc@gmail.com>
2017-01-30 14:52:10 -0800

    Created AHRSGyro Class
    Impemented methods and added variables

---
+++ NRGRobot2017/src/org/usfirst/frc/team948/utilities/AHRSGyro.java
@@ -0,0 +1,99 @@
+package org.usfirst.frc.team948.utilities;
+
+import edu.wpi.first.wpilibj.PIDSource;
+import edu.wpi.first.wpilibj.PIDSourceType;
+import edu.wpi.first.wpilibj.interfaces.Gyro;
+import edu.wpi.first.wpilibj.livewindow.LiveWindowSendable;
+import edu.wpi.first.wpilibj.tables.ITable;
+
+public class AHRSGyro implements Gyro, PIDSource, LiveWindowSendable {
+	private double previousAngle;
+	private double currentAngle;
+	private double angleOffset; 
+	
+
+	@Override
+	public void initTable(ITable subtable) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public ITable getTable() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getSmartDashboardType() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void updateTable() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void startLiveWindowMode() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void stopLiveWindowMode() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void setPIDSourceType(PIDSourceType pidSource) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public PIDSourceType getPIDSourceType() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public double pidGet() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public void calibrate() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void reset() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public double getAngle() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public double getRate() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public void free() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
