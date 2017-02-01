7b7731b4c84d0a9bc9ac5c4a2052fb20a8d29c93
ScottSun42 <scott.s4116@gmail.com>
2017-01-31 19:45:52 -0800

    #45 Create Dummy Shooter Subsystem
    Scott Sun, Tarun Challa, Rock Jia

---
+++ NRGRobot2017/src/org/usfirst/frc/team948/robot/subsystems/Shooter.java
@@ -0,0 +1,13 @@
+package org.usfirst.frc.team948.robot.subsystems;
+
+import org.usfirst.frc.team948.robot.RobotMap;
+
+/**
+ * 
+ */
+
+public class Shooter extends Subsystem {
+	
+	//Put methods for controlling this subsystem
+
+}
