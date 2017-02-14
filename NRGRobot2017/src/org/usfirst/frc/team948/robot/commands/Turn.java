550df19b1650d1e229c6a8c38c0ea8badb551e5b
Nipun Chhajed <nipunchhajed@gmail.com>
2017-02-13 19:12:20 -0800

    fixed errors in turn command

--- NRGRobot2017/src/org/usfirst/frc/team948/robot/commands/Turn.java
+++ NRGRobot2017/src/org/usfirst/frc/team948/robot/commands/Turn.java
@@ -21,22 +21,22 @@ public class Turn extends Command {
 
 	// Called just before this Command runs the first time
 	protected void initialize() {
-		Robot.drive.turnToHeadingInit2(desiredHeading);
+		Robot.drive.turnToHeadingInitNoPID(desiredHeading);
 	}
 
 	// Called repeatedly when this Command is scheduled to run
 	protected void execute() {
-		Robot.drive.turnToHeading2(power);
+		Robot.drive.turnToHeadingNoPID(power);
 	}
 
 	// Make this return true when this Command no longer needs to run execute()
 	protected boolean isFinished() {
-		return Robot.drive.isOnHeading2();
+		return Robot.drive.isOnHeadingNoPID();
 	}
 
 	// Called once after isFinished returns true
 	protected void end() {
-		Robot.drive.turnToHeadingEnd2(desiredHeading);
+		Robot.drive.turnToHeadingEndNoPID(desiredHeading);
 	}
 
 	// Called when another command which requires one or more of the same
