767c9ae4035b011857e3f8b8b97a33e36789240e
gregoryquick <gregory@quicks.me>
2017-02-18 14:26:38 -0800

    Changed conversion for the distance units in simple vision routine
    The second drive strait had an incorect slaing factor...

--- NRGRobot2017/src/org/usfirst/frc/team948/robot/commandgroups/simpleVisionRoutine.java
+++ NRGRobot2017/src/org/usfirst/frc/team948/robot/commandgroups/simpleVisionRoutine.java
@@ -12,6 +12,7 @@ import org.usfirst.frc.team948.utilities.visionProc;
 import edu.wpi.first.wpilibj.command.CommandGroup;
 
 public class simpleVisionRoutine extends CommandGroup {
+	public static final double scalar = 1/12.0;
 	visionField a;
 	public simpleVisionRoutine(visionProc proc){
 		addSequential(new FlipCameraLight(true));
@@ -21,10 +22,10 @@ public class simpleVisionRoutine extends CommandGroup {
 			addSequential(new ShiftGears(false));
 			if(a.zeta != 0){
 				addSequential(new Turn(90.0*a.zeta, 0.5));
-				addSequential(new DriveStraightDistance(Math.abs(a.zeta)/12,0.5));
+				addSequential(new DriveStraightDistance(Math.abs(a.zeta)*scalar,0.5));
 				addSequential(new Turn(-90.0*a.zeta,0.5));
 			}
-			addSequential(new DriveStraightDistance(Math.abs(a.v/2),0.5));
+			addSequential(new DriveStraightDistance(Math.abs(a.v)*scalar,0.5));
 		}
 		addSequential(new FlipCameraLight(false));
 	}
