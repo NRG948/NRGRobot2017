ff38d711b32121888f5a4da6fe5d1a30e66aee54
DJ Wadhwa <wadhwadhananjay@gmail.com>
2017-03-25 13:43:02 -0700

    Clean up ShootSequence

--- NRGRobot2017/src/org/usfirst/frc/team948/robot/commandgroups/ShootSequence.java
+++ NRGRobot2017/src/org/usfirst/frc/team948/robot/commandgroups/ShootSequence.java
@@ -1,20 +1,17 @@
 package org.usfirst.frc.team948.robot.commandgroups;
 
 import org.usfirst.frc.team948.robot.commands.FeedBall;
-import org.usfirst.frc.team948.robot.commands.Shoot;
 import org.usfirst.frc.team948.robot.commands.SpinShooterToRPM;
 
 import edu.wpi.first.wpilibj.command.CommandGroup;
 
 /**
- *
+ * Starts shooting automatically after the shooter wheel hits the target RPM.
  */
 public class ShootSequence extends CommandGroup {
 	public ShootSequence() {
 		addParallel(new SpinShooterToRPM());
 		// addParallel(new Shoot());
 		addSequential(new FeedBall(.5, true));
-
 	}
-
 }
