2a07ce5c1f531fa4160790ea37aa7fbc504033cd
DJ Wadhwa <wadhwadhananjay@gmail.com>
2017-03-25 15:31:56 -0700

    Add BallFeeder.start() with default power

--- NRGRobot2017/src/org/usfirst/frc/team948/robot/subsystems/BallFeeder.java
+++ NRGRobot2017/src/org/usfirst/frc/team948/robot/subsystems/BallFeeder.java
@@ -6,21 +6,24 @@ import edu.wpi.first.wpilibj.command.Subsystem;
 
 public class BallFeeder extends Subsystem {
 
+	private static final double DEFAULT_CONVEYOR_POWER = 0.5;
+	
 	@Override
 	protected void initDefaultCommand() {
-		// TODO Auto-generated method stub
-
 	}
 
-	public void start(double power) {
+	public void start() {
+		start(DEFAULT_CONVEYOR_POWER);
+	}
+
+	public void start(double conveyorPower) {
 		// bottom motor ejects ball with negative power;
 		RobotMap.shooterWheelBottom.set(-1);
-		RobotMap.conveyorBelt.set(power);
+		RobotMap.conveyorBelt.set(conveyorPower);
 	}
 
 	public void stop() {
 		RobotMap.shooterWheelBottom.disable();
 		RobotMap.conveyorBelt.disable();
 	}
-
 }
