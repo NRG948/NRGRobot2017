f96792887b3763839ddcd268461c777cf22d0186
DJ Wadhwa <wadhwadhananjay@gmail.com>
2017-03-25 13:48:32 -0700

    New command group which shoots at boiler after dropping a gear
    Not there yet, but eventually this should work for boiler-side or center
    auto positions.

---
+++ NRGRobot2017/src/org/usfirst/frc/team948/robot/commandgroups/ShootAfterGearDropOff.java
@@ -0,0 +1,28 @@
+package org.usfirst.frc.team948.robot.commandgroups;
+
+import static org.usfirst.frc.team948.robot.subsystems.Drive.Direction.BACKWARD;
+
+import org.usfirst.frc.team948.robot.commands.DriveStraightDistance;
+import org.usfirst.frc.team948.robot.commands.FeedBall;
+import org.usfirst.frc.team948.robot.commands.SetInitPoz;
+import org.usfirst.frc.team948.robot.commands.SpinShooterToRPM;
+import org.usfirst.frc.team948.robot.commands.TurnToHeading;
+import org.usfirst.frc.team948.robot.commands.WaitUntilGearDrop;
+
+import edu.wpi.first.wpilibj.command.CommandGroup;
+
+/**
+ *
+ */
+public class ShootAfterGearDropOff extends CommandGroup {
+	private static final int SHOOTER_RPM_NEAR_PEG = 3100;
+	
+	public ShootAfterGearDropOff(double delayTime) {
+		addSequential(new SetInitPoz(0, 0, 60)); // test parameters; remove later
+		addParallel(new SpinShooterToRPM(SHOOTER_RPM_NEAR_PEG));
+		addSequential(new WaitUntilGearDrop(delayTime));
+		addSequential(new DriveStraightDistance(55, BACKWARD));
+		addSequential(new TurnToHeading(165));
+		addSequential(new FeedBall());
+	}
+}
