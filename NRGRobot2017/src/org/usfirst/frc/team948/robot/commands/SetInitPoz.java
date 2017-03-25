2fc312e9cc56531a34cd06519f69679758a12b09
DJ Wadhwa <wadhwadhananjay@gmail.com>
2017-03-25 13:50:32 -0700

    Change SetInitPoz command to take an optional gyro offset

--- NRGRobot2017/src/org/usfirst/frc/team948/robot/commands/SetInitPoz.java
+++ NRGRobot2017/src/org/usfirst/frc/team948/robot/commands/SetInitPoz.java
@@ -1,6 +1,7 @@
 package org.usfirst.frc.team948.robot.commands;
 
 import org.usfirst.frc.team948.robot.Robot;
+import org.usfirst.frc.team948.robot.RobotMap;
 
 import edu.wpi.first.wpilibj.command.Command;
 
@@ -10,15 +11,22 @@ import edu.wpi.first.wpilibj.command.Command;
 public class SetInitPoz extends Command {
 	private double x;
 	private double y;
+	private double initHeading;
 
 	public SetInitPoz(double x, double y) {
+		this(x, y, 0);
+	}
+	
+	public SetInitPoz(double x, double y, double initHeading) {
 		this.x = x;
 		this.y = y;
+		this.initHeading = initHeading;
 	}
 
 	// Called just before this Command runs the first time
 	protected void initialize() {
 		Robot.positionTracker.setXY(x, y);
+		RobotMap.continuousGyro.setHeadingOffset(initHeading);
 	}
 
 	// Make this return true when this Command no longer needs to run execute()
