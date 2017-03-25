fae6f0d780a31326bb0e0ef73b56ff9aad13aab2
DJ Wadhwa <wadhwadhananjay@gmail.com>
2017-03-25 14:48:41 -0700

    OI cleanups and additions
    1) rpmShooter button on Arduino has reversed action ("ON" position reads
    as off)
    2) Shoot button action changed to use whenPressed and whenReleased.
    3) Put joystick buttons in numerical order.

--- NRGRobot2017/src/org/usfirst/frc/team948/robot/OI.java
+++ NRGRobot2017/src/org/usfirst/frc/team948/robot/OI.java
@@ -4,6 +4,7 @@ import org.usfirst.frc.team948.robot.Robot.AutoMovement;
 import org.usfirst.frc.team948.robot.Robot.AutoPosition;
 import org.usfirst.frc.team948.robot.commandgroups.AutonomousRoutines;
 import org.usfirst.frc.team948.robot.commandgroups.PressToPeg;
+import org.usfirst.frc.team948.robot.commandgroups.ShootAfterGearDropOff;
 import org.usfirst.frc.team948.robot.commandgroups.ShootSequence;
 import org.usfirst.frc.team948.robot.commands.BallCollect;
 import org.usfirst.frc.team948.robot.commands.ClimbPower;
@@ -14,15 +15,11 @@ import org.usfirst.frc.team948.robot.commands.ManualDriveStraight;
 import org.usfirst.frc.team948.robot.commands.ResetSensors;
 import org.usfirst.frc.team948.robot.commands.ShiftGears;
 import org.usfirst.frc.team948.robot.commands.Shoot;
-import org.usfirst.frc.team948.robot.commands.StopTestDrive;
-import org.usfirst.frc.team948.robot.commands.TestDrive;
 import org.usfirst.frc.team948.robot.commands.SpinShooterToRPM;
-import org.usfirst.frc.team948.robot.subsystems.Shooter;
 import org.usfirst.frc.team948.utilities.PreferenceKeys;
 
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.Joystick;
-import edu.wpi.first.wpilibj.buttons.Button;
 import edu.wpi.first.wpilibj.buttons.JoystickButton;
 
 /**
@@ -35,36 +32,36 @@ public class OI {
 	public static Joystick rightJoystick = new Joystick(2);
 	public static JoystickButton manualDriveStraight = new JoystickButton(leftJoystick, 1);
 	public static JoystickButton interruptButton = new JoystickButton(leftJoystick, 2);
-	public static JoystickButton resetSensorsButton = new JoystickButton(leftJoystick, 11);
+	public static JoystickButton pressToPeg = new JoystickButton(leftJoystick, 3);
 	public static JoystickButton climberForwards = new JoystickButton(leftJoystick, 6);
 	public static JoystickButton climberReverse = new JoystickButton(leftJoystick, 7);
 	public static JoystickButton cameraLightSwitch = new JoystickButton(leftJoystick, 8);
-	public static JoystickButton pressToPeg = new JoystickButton(leftJoystick, 3);
 	public static JoystickButton testShooterRPM = new JoystickButton(leftJoystick, 9);
+	public static JoystickButton resetSensorsButton = new JoystickButton(leftJoystick, 11);
 	
 	public static JoystickButton rightTrigger = new JoystickButton(rightJoystick, 1);
+	public static JoystickButton ShootingAuto = new JoystickButton(rightJoystick, 2);
 	public static JoystickButton shoot = new JoystickButton(rightJoystick, 3);
-	public static JoystickButton acquireBalls = new JoystickButton(rightJoystick, 7);
 	public static JoystickButton ejectBalls = new JoystickButton(rightJoystick, 6);
+	public static JoystickButton acquireBalls = new JoystickButton(rightJoystick, 7);
+	public static JoystickButton testShootAfterGearDrop = new JoystickButton(rightJoystick, 8); // test button
 	public static JoystickButton shootSequence = new JoystickButton(rightJoystick, 9);
 	public static JoystickButton switchLowGear = new JoystickButton(rightJoystick, 10);
 	public static JoystickButton switchHighGear = new JoystickButton(rightJoystick, 11);
-	public static JoystickButton ShootingAuto = new JoystickButton(rightJoystick, 2);
 
 	public static final JoystickButton climberButton = new JoystickButton(arduinoJoystick, 1);
 	public static final JoystickButton driveWithVision = new JoystickButton(arduinoJoystick, 3);
 	public static final JoystickButton autoLeft = new JoystickButton(arduinoJoystick, 4);
-	// the middle position is when both the left and right button states are
-	// false
+	// The middle position is when both the left and right button states are false
 	public static final JoystickButton autoMiddle = new JoystickButton(arduinoJoystick, 5);
 	public static final JoystickButton autoRight = new JoystickButton(arduinoJoystick, 6);
-	public static final JoystickButton stay = new JoystickButton(arduinoJoystick, 10);
-	public static final JoystickButton driveAirship = new JoystickButton(arduinoJoystick, 9);
-	public static final JoystickButton driveShort = new JoystickButton(arduinoJoystick, 8);
 	public static final JoystickButton driveLong = new JoystickButton(arduinoJoystick, 7);
+	public static final JoystickButton driveShort = new JoystickButton(arduinoJoystick, 8);
+	public static final JoystickButton driveAirship = new JoystickButton(arduinoJoystick, 9);
+	public static final JoystickButton stay = new JoystickButton(arduinoJoystick, 10);
 	public static final JoystickButton acquirerForward = new JoystickButton(arduinoJoystick, 11);
 	public static final JoystickButton acquirerBackward = new JoystickButton(arduinoJoystick, 13);
-	public static final JoystickButton feeder = new JoystickButton(arduinoJoystick, 14);
+	public static final JoystickButton feedShooter = new JoystickButton(arduinoJoystick, 14);
 	public static final JoystickButton rpmShooter = new JoystickButton(arduinoJoystick, 15);
 
 	public static void buttonInit() {
@@ -85,11 +82,13 @@ public class OI {
 		shootSequence.whenPressed(new ShootSequence());
 		acquirerForward.whenPressed(new BallCollect(true));
 		acquirerBackward.whenPressed(new BallCollect(false));
-		feeder.toggleWhenActive(new FeedBall(0.5, false));
-		rpmShooter.whenPressed(new SpinShooterToRPM());
+		feedShooter.whenPressed(new FeedBall());
+		feedShooter.whenReleased(new FeedBall(0.0, false));
+		rpmShooter.whenReleased(new SpinShooterToRPM());
 		ejectBalls.toggleWhenActive(new BallCollect(false));
 		acquireBalls.toggleWhenActive(new BallCollect(true));
 		ShootingAuto.whenPressed(new AutonomousRoutines.BlueLeftShooter(1));
+		testShootAfterGearDrop.whenPressed(new ShootAfterGearDropOff(1));
 	}
 
 	public static AutoPosition getAutoPosition() {
