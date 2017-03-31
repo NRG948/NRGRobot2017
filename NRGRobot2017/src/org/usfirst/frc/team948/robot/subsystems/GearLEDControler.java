7f00b5e4adda99482bbaa8b49be4770819598f7b
chandrap2 <chandra2000@outlook.com>
2017-03-30 17:05:57 -0700

    Took out if-statement to check for gettingData
    took out extraneous code

--- NRGRobot2017/src/org/usfirst/frc/team948/robot/subsystems/GearLEDControler.java
+++ NRGRobot2017/src/org/usfirst/frc/team948/robot/subsystems/GearLEDControler.java
@@ -7,66 +7,56 @@ import org.usfirst.frc.team948.utilities.MultiChannelLED;
  *
  */
 public class GearLEDControler {
-
 	private MultiChannelLED gearLEDs;
 	private boolean gettingData = false;
 	private boolean onTarget = false;
 	
-	public GearLEDControler(MultiChannelLED gearLEDs){
+	public GearLEDControler(MultiChannelLED gearLEDs) {
 		this.gearLEDs = gearLEDs;
 	}
 	
-	public void updateLights(){
-		if(!RobotMap.lowerGearSensor.get()){
+	public void updateLights() {
+		if (!RobotMap.lowerGearSensor.get()) {
 			gearLEDs.turnBlueOn();
-			if(gettingData){
-				if(onTarget){
-					gearLEDs.turnRedOn();
-				}else{
-					gearLEDs.turnRedOff();
-				}
-			}else{
-				gearLEDs.turnRedOff();
+			
+			if (onTarget) {
+				gearLEDs.turnRedOn();
+			} else {
+				gearLEDs.turnRedOff(); // not on target OR not getting data
 			}
+			
 			gearLEDs.turnGreenOff();
-		}else if(!RobotMap.upperGearSensor.get()){
+		} else if (!RobotMap.upperGearSensor.get()) {
 			gearLEDs.turnRedOn();
-			if(gettingData){
-				if(onTarget){
-					gearLEDs.turnGreenOn();
-				}else{
-					gearLEDs.turnGreenOff();
-				}
-			}else{
+			
+			if (onTarget) {
+				gearLEDs.turnGreenOn();
+			} else {
 				gearLEDs.turnGreenOff();
 			}
+			
 			gearLEDs.turnBlueOff();
-		}else{
+		} else {
 			gearLEDs.turnBlueOff();
 			gearLEDs.turnRedOff();
-			if(gettingData){
-				if(onTarget){
-					gearLEDs.turnGreenOn();
-				}else{
-					gearLEDs.turnGreenOff();
-				}
-			}else{
+			
+			if (onTarget) {
+				gearLEDs.turnGreenOn();
+			} else {
 				gearLEDs.turnGreenOff();
 			}
 		}
 	}
 	
-	public void setGettingData(boolean isGettingData){
+	public void setGettingData(boolean isGettingData) {
 		gettingData = isGettingData;
 	}
 	
-	public void writeData(boolean isOnTarget){
-		if(gettingData){
+	public void writeData(boolean isOnTarget) {
+		if(gettingData) {
 			onTarget = isOnTarget;
-		}else{
+		} else {
 			System.out.println("You need to set gettingData to true...");
 		}
 	}
-	
 }
-
